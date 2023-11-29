2b439d655f3af9a203991addecc0943ab4700151
ksmykala <k.smykala@gmail.com>
2023-11-29 20:12:47 +0100

    SECTION 5 challenge: profile update

--- 03-pizza-menu/03-PROFILE/profile/src/index.js
+++ 03-pizza-menu/03-PROFILE/profile/src/index.js
@@ -3,7 +3,45 @@ import { createRoot } from "react-dom/client";
 
 import "./styles.css";
 
-// import App from "./App";
+const skills = [
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#FFCE00"
+  },
+  {
+    skill: "HTML",
+    level: "beginner",
+    color: "#0090ff"
+  },
+  {
+    skill: "CSS",
+    level: "intermediate",
+    color: "#800080"
+  },
+  {
+    skill: "Python",
+    level: "advanced",
+    color: "#FF0000"
+  },
+  {
+    skill: "Java",
+    level: "beginner",
+    color: "#007396"
+  },
+  {
+    skill: "C#",
+    level: "advanced",
+    color: "#649AD2"
+  },
+  {
+    skill: "Ruby",
+    level: "beginner",
+    color: "#CC342D"
+  }
+];
+
+
 
 function Avatar() {
   return (
@@ -28,32 +66,39 @@ function Intro() {
 }
 
 function SkillList() {
+  const sortedSkills = skills.sort((a, b) => {
+    return a.skill.localeCompare(b.skill);
+  });
+
   return (
     <div className="skill-list">
-      <Skill 
-        skillname="Git"
-        icon="💪"
-        color="red"
-      />
-      <Skill
-        skillname="Saving the world"
-        icon="💪"
-        color="orange"
-      />
-      <Skill
-        skillname="Cooking"
-        icon="🍗"
-        color="limegreen"
-      />
+      {skills.length > 0 ? (
+        sortedSkills.map((skill) => (
+          <Skill
+            skillname={skill.skill}
+            level={skill.level}
+            color={skill.color}
+            key={skill.skill}
+          />
+        ))
+      ) : (
+        <p>I'm still working on my portfolio. Please come back later :)</p>
+      )}
     </div>
   );
 }
 
-function Skill(data) {
+function Skill({skillname, level, color}) {
+  const levelToEmoji = {
+    beginner: "👶",
+    intermediate: "👍",
+    advanced: "💪"
+  };
+
   return (
-    <div className="skill" style={{backgroundColor: data.color}}>
-      <span>{data.skillname}</span>
-      <span>{data.icon}</span>
+    <div className="skill" style={{backgroundColor: color}}>
+      <span>{skillname}</span>
+      <span>{levelToEmoji[level]}</span>
     </div>
   );
 }
