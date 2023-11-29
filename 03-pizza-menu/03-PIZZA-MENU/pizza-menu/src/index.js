d3657df52c62301e8b780832ac7f7d130c195bb8
ksmykala <k.smykala@gmail.com>
2023-11-29 19:43:25 +0100

    Finish section 5

--- 03-pizza-menu/03-PIZZA-MENU/pizza-menu/src/index.js
+++ 03-pizza-menu/03-PIZZA-MENU/pizza-menu/src/index.js
@@ -89,18 +89,14 @@ function Menu() {
   );
 }
 
-function Pizza({pizzaObj}) {
-  console.log(pizzaObj);
-
-  // if (props.pizzaObj.soldOut) return null;
-
+function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza${pizzaObj.soldOut ? " sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
