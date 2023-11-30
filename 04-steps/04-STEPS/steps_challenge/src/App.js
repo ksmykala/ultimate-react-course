67ecb40bebf7a224bd087d77844c61b87c2d3cf0
ksmykala <k.smykala@gmail.com>
2023-11-30 14:43:23 +0100

    Add date counting

--- 04-steps/04-STEPS/steps_challenge/src/App.js
+++ 04-steps/04-STEPS/steps_challenge/src/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 
 function App() {
   const [step, setStep] = useState(0);
   const [count, setCount] = useState(0);
+  const [targetDate, setTargetDate] = useState("")
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const targetDate = new Date(currentDate.getTime() + count * 24 * 60 * 60 * 1000);
+    const month = targetDate.getMonth() + 1;
+    const formattedMonth = month < 10 ? `0${month}` : month;
+    const formattedTargetDate = `${targetDate.getDate()}.${formattedMonth}.${targetDate.getFullYear()}`;
+    setTargetDate(formattedTargetDate);
+  }, [step, count]);
 
   return (
     <div className="main">
       <Counter title="Step" s_value={step} setFunc={setStep} />
       <Counter title="Count" s_value={count} setFunc={setCount} multiplier={step} />
+      <div className="counter-row">
+        <div className="counter">
+        <h1>{count} days from today is {targetDate}</h1>
+        </div>
+      </div>
     </div>
   );
 }
