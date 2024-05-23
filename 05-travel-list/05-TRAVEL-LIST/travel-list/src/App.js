ded7a7f27bd4ed1436086852c5781d1727103941
Krzysztof Smykała <ksmykala@qzsolutions.pl>
2024-05-23 14:15:45 +0200

    Travel list - another part

--- 05-travel-list/05-TRAVEL-LIST/travel-list/src/App.js
+++ 05-travel-list/05-TRAVEL-LIST/travel-list/src/App.js
@@ -6,11 +6,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackageList />
+      <Form onAddItems={handleAddItem} />
+      <PackageList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +26,7 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -36,6 +42,8 @@ function Form() {
       id: Date.now(),
     };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -65,11 +73,11 @@ function Form() {
   );
 }
 
-function PackageList() {
+function PackageList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
