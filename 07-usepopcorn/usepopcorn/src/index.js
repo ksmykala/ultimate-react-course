89d6d4512c575864f6fe7e40f97a645fa51ae9a5
Krzysztof Smykała <ksmykala@qzsolutions.pl>
2024-06-19 21:53:57 +0200

    Reusibility with props

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -4,10 +4,25 @@ import ReactDOM from "react-dom/client";
 // import App from './App';
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = React.useState(0);
+  return (
+    <div>
+      <StarRating color="coral" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} start</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} color="limegreen" size={24} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
